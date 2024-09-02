c21c096a5f1665ffc66fd97b730e3b738f63f0f8
me-lucas-al <lucasalsouza2006@gmail.com>
2024-09-02 03:01:10 -0300

    chat-main

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -11,19 +11,11 @@ export default function Chat({ socket }) {
   const [userColors, setUserColors] = useState({});
 
   useEffect(() => {
-    const savedMessages = JSON.parse(localStorage.getItem('messages')) || [];
-    setMessageList(savedMessages);
-
     socket.on('receive_message', (data) => {
       if (Array.isArray(data)) {
         setMessageList(data);
-        localStorage.setItem('messages', JSON.stringify(data));
       } else {
-        setMessageList((current) => {
-          const newList = [...current, data];
-          localStorage.setItem('messages', JSON.stringify(newList));
-          return newList;
-        });
+        setMessageList((current) => [...current, data]);
       }
     });
 
@@ -80,7 +72,7 @@ export default function Chat({ socket }) {
 
   return (
     <div>
-      <img src={logo} alt="logo" style={{ width: '300px', height: 'auto', marginLeft: '60px', cursor: 'pointer' }} onClick={() => navigate('/')} />
+      <img src={logo} alt="logo" style={{ width: '300px', height: 'auto', marginLeft: '15%', cursor: 'pointer'}} onClick={() => navigate('/')} />
       <div className={style['chat-container']}>
         <div className={style['chat-body']}>
           {messageList.map((message, index) => (
@@ -102,6 +94,7 @@ export default function Chat({ socket }) {
                 <strong>{message.authorId === socket.id ? 'Você' : message.author}</strong>
               </div>
               <div className="message-text">{message.text}</div>
+
             </div>
           ))}
           <div ref={bottomRef} />
